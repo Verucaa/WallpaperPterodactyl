6fc93e162abfe7b7dece41546e81a59084dbe733
Verucaa <udangketce@gmail.com>
2025-02-28 14:26:46 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ installTheme(){
     echo "Installing theme..."
     cd /var/www/pterodactyl
     rm -r Utama
-    git clone https://github.com/arzanoffc1/Utama.git
+    git clone https://github.com/Verucaa/Utama.git
     cd Utama
     rm /var/www/pterodactyl/resources/scripts/Utama.css
     rm /var/www/pterodactyl/resources/scripts/index.tsx
@@ -47,7 +47,7 @@ installThemeQuestion(){
 }
 
 repair(){
-    bash <(curl https://raw.githubusercontent.com/arzanoffc1/Utama/main/repair.sh)
+    bash <(curl https://raw.githubusercontent.com/Verucaa/WallpaperPterodactyl/main/repair.sh)
 }
 
 restoreBackUp(){
